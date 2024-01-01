be1abdc3ff9ffdadee92c2503727bbb4f1c0d118
Jiyoon Lim <luinpark@gmail.com>
2024-01-01 23:24:36 +0900

    FEATURE: draw two Dice

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import { Canvas } from '@react-three/fiber'
+import Dice from './components/Dice.tsx'
 import Ground from './components/Ground.tsx'
+import {Physics, useBox} from "@react-three/cannon";
+import PointLight from "./components/PointLight.tsx";
 import './styles/App.css'
 import {OrbitControls} from "@react-three/drei";
 
@@ -12,11 +15,27 @@ export function App() {
           position: [0, 10, 10],
         }}
       >
+        {/*<ambientLight />*/}
+        <PointLight />
         <Physics gravity={[0, -9.81, 0]}>
           <Ground scale={groundSize} />
+          <group>
+            <Dice
+              position={[0, 5, 0]}
+              rotation={[0.4, 0.24, 0.15]}
+              scale={[1, 1, 1]}
+            />
+            <Dice
+              position={[0.1, 6, 0]}
+              rotation={[0.0, 0.0, 0.0]}
+              scale={[1, 1, 1]}
+            />
+          </group>
 
         </Physics>
         <OrbitControls />
+        <axesHelper args={[groundSize.width]} />
+        <gridHelper args={[groundSize.width, groundSize.height]} />
       </Canvas>
     </>
   )
