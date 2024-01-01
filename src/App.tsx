e9bfaefd2f3dd359bf96ebeed60c31009ab23bd8
Jiyoon Lim <luinpark@gmail.com>
2024-01-01 23:20:35 +0900

    FEATURE: add canvas and camera

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,16 @@
+import { Canvas } from '@react-three/fiber'
 import './styles/App.css'
 
 export function App() {
+  const groundSize = {"width": 20, "height": 20}
+  return (
+    <>
+      <Canvas
+        camera={{
+          position: [0, 10, 10],
+        }}
+      >
+      </Canvas>
+    </>
+  )
 }
